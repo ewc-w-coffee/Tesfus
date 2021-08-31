f862f631211d54a7b041a46799ac9390cbba62aa
tsolutionsx <techsolutionsx7419@gmail.com>
2021-08-31 12:30:42 +0000

    feture : some component

---
+++ components/Container/index.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding-left: 2.4rem;
+  padding-right: 2.4rem;
+  max-width: 128rem;
+  margin-left: auto;
+  margin-right: auto;
+`;

3f5136ebba091de53a48a0b2d33f27f7ef336e62
tsolutionsx <techsolutionsx7419@gmail.com>
2021-08-31 12:21:51 +0000

    Initial structure

--- components/Container/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  padding-left: 2.4rem;
-  padding-right: 2.4rem;
-  max-width: 128rem;
-  margin-left: auto;
-  margin-right: auto;
-`;
--- styles/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as GlobalStyle } from "./global";
-export { default as theme } from "./theme";
-export { default as media } from "./media";

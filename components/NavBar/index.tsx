1481b7095cee304b84b087032980030302bcad39
tsolutionsx <techsolutionsx7419@gmail.com>
2021-08-31 13:48:58 +0000

    feture: ASD980012UO

---
+++ components/NavBar/index.tsx
@@ -0,0 +1,127 @@
+import {
+  LoginBtn,
+  NavBox,
+  NavContainer,
+  NavContent,
+  NavItem,
+  NavLogo,
+  ProfileModal,
+  ProfileModalItem,
+  RightContainer,
+  UserBox,
+  UserImg,
+  UserName,
+} from "./styles";
+import Link from "next/link";
+import DiscordIcon from "../../public/img/discord.svg";
+import { useRouter } from "next/router";
+import qs from "query-string";
+import { useContext, useEffect } from "react";
+import { LoginContext } from "context/LoginContext";
+import { useState } from "react";
+
+const NavBar = () => {
+  const router = useRouter();
+
+  const [showModal, setShowModal] = useState(false);
+
+  const {
+    setDiscordToken,
+    user,
+    setUser,
+    checkIfBuyer,
+    isBuyer,
+    joinBuyersServer,
+    joinCommunityServer,
+  } = useContext(LoginContext);
+
+  useEffect(() => {
+    if (user) {
+      checkIfBuyer();
+    }
+  }, [user, checkIfBuyer]);
+
+  const genDiscordUrl = () => {
+    const query = {
+      client_id: process.env.CLIENT_ID,
+      scope: ["identify", "guilds", "guilds.join"].join(" "),
+      response_type: "code",
+      redirect_uri: process.env.REDIRECT_URI,
+    };
+
+    return qs.stringifyUrl({
+      url: "https://discordapp.com/api/oauth2/authorize",
+      query: query,
+    });
+  };
+
+  const logout = () => {
+    localStorage.removeItem("discordToken");
+    if (setDiscordToken) {
+      setDiscordToken(undefined);
+    }
+    if (setUser) {
+      setUser(undefined);
+    }
+
+    setShowModal(false);
+  };
+
+  return (
+    <NavContainer>
+      <NavContent>
+        <NavBox>
+          {showModal && (
+            <ProfileModal>
+              {isBuyer && (
+                <ProfileModalItem onClick={joinBuyersServer}>
+                  Buyers Server
+                </ProfileModalItem>
+              )}
+              <ProfileModalItem onClick={joinCommunityServer}>
+                Community Server
+              </ProfileModalItem>
+              <ProfileModalItem onClick={logout}>Logout</ProfileModalItem>
+            </ProfileModal>
+          )}
+          <Link href="/">
+            <a>
+              <NavLogo src="/img/logo.svg" />
+            </a>
+          </Link>
+          <Link href="/shop">
+            <a>
+              <NavItem className={router.pathname == "/shop" ? "active" : ""}>
+                Shop
+              </NavItem>
+            </a>
+          </Link>
+          <RightContainer>
+            {!user && (
+              <Link href={genDiscordUrl()}>
+                <a>
+                  <LoginBtn>
+                    <DiscordIcon />
+                    <span>Login</span>
+                  </LoginBtn>
+                </a>
+              </Link>
+            )}
+            {user && (
+              <UserBox onClick={() => setShowModal(!showModal)}>
+                <UserName>
+                  {user.username}#{user.discriminator}
+                </UserName>
+                <UserImg
+                  src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
+                />
+              </UserBox>
+            )}
+          </RightContainer>
+        </NavBox>
+      </NavContent>
+    </NavContainer>
+  );
+};
+
+export default NavBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,2 +1,204 @@
 import { ThemeProvider } from "styled-components";
 import { AppProps } from "next/app";
+import { GlobalStyle, theme } from "@styles";
+import { LoginContext } from "context/LoginContext";
+import { useEffect, useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { Slide, toast, ToastContainer } from "react-toastify";
+import axios from "axios";
+import firebase from "firebase";
+import Head from "next/head";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyAjvZlHqrGAsuvcp014TALnBc81NB6Ave0",
+    authDomain: "teefus-884db.firebaseapp.com",
+    databaseURL: "https://teefus-884db-default-rtdb.firebaseio.com",
+    projectId: "teefus-884db",
+    storageBucket: "teefus-884db.appspot.com",
+    messagingSenderId: "1091692170705",
+    appId: "1:1091692170705:web:e7c4b196af6d15fdb5a4f0",
+    measurementId: "G-WGBPSPG81M",
+  });
+} else {
+  firebase.app(); // if already initialized, use that one
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [discordToken, setDiscordToken] = useState<string | undefined>();
+  const [user, setUser] = useState<any>();
+  const [isBuyer, setIsBuyer] = useState(false);
+  const [inCommunity, setInCommunity] = useState(false);
+
+  async function checkInCommunity() {
+    try {
+      const { data: guilds } = await axios.get(
+        "https://discordapp.com/api/v6/users/@me/guilds",
+        {
+          headers: {
+            Authorization: `Bearer ${discordToken}`,
+          },
+        }
+      );
+      setInCommunity(
+        guilds.some((guild: any) => guild.name === "Teefus Community")
+      );
+    } catch (error) {
+      setInCommunity(false);
+      console.log(error);
+    }
+  }
+
+  async function joinBuyersServer() {
+    console.log(user + "  user");
+    axios
+      .put(
+        `https://discordapp.com/api/v6/guilds/867247619305766912/members/${user.id}`,
+        {
+          access_token: discordToken,
+        },
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bot ${process.env.BOT_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        if (response.status == 204) {
+          toast.error("You are in the server already.");
+        } else {
+          toast.success("You have joined the Buyers Server.");
+        }
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
+  }
+  async function joinCommunityServer() {
+    axios
+      .put(
+        `https://discordapp.com/api/v6/guilds/881228508163092490/members/${user.id}`,
+        {
+          access_token: discordToken,
+        },
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bot ${process.env.BOT_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        setInCommunity(true);
+        if (response.status == 204) {
+          toast.error("You are in the server already.");
+        } else {
+          toast.success("You have joined the Community Server.");
+        }
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
+  }
+  async function giveBuyersRole() {
+    await axios
+      .put(
+        `https://discordapp.com/api/v6/guilds/881228508163092490/members/${user.id}/roles/881457904153153557`,
+        {
+          access_token: discordToken,
+        },
+        {
+          headers: {
+            Authorization: `Bot ${process.env.BOT_TOKEN}`,
+          },
+        }
+      )
+      .catch(() => {
+        //localStorage.setItem('')
+      });
+  }
+
+  async function checkIfBuyer() {
+    try {
+      const roles = (
+        await axios.get(
+          `https://discordapp.com/api/v6/guilds/881228508163092490/members/${user.id}`,
+          {
+            headers: {
+              Authorization: `Bot ${process.env.BOT_TOKEN}`,
+            },
+          }
+        )
+      ).data.roles;
+      setIsBuyer(roles.includes("881457904153153557"));
+    } catch (error) {
+      setIsBuyer(false);
+      console.log(error);
+    }
+  }
+
+  const getUser = async (token: string) => {
+    try {
+      const { data } = await axios.get("https://discordapp.com/api/users/@me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUser(data);
+    } catch (error) {
+      setUser(undefined);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    let localToken = localStorage.getItem("discordToken");
+
+    console.log(localToken);
+
+    if (localToken) {
+      setDiscordToken(localToken);
+      getUser(localToken);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      checkIfBuyer();
+    }
+  }, [user]);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <LoginContext.Provider
+          value={{
+            discordToken,
+            setDiscordToken,
+            user,
+            setUser,
+            joinBuyersServer,
+            giveBuyersRole,
+            checkIfBuyer,
+            isBuyer,
+            setIsBuyer,
+            joinCommunityServer,
+            setInCommunity,
+            inCommunity,
+            checkInCommunity,
+          }}
+        >
+          <Component {...pageProps} />
+          <ToastContainer transition={Slide} position="bottom-right" />
+        </LoginContext.Provider>
+      </ThemeProvider>
+    </>
+  );
+}
